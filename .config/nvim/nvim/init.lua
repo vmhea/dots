2f60e242901d040b9c1258d8930511d3be5dd00e
vmhea <hvernet93@gmail.com>
2022-04-04 22:52:01 -0400

    Fixed redundant directories created via faulty update script.

--- .config/nvim/nvim/init.lua
+++
@@ -1,8 +0,0 @@
-require('packer_init')
-
-require('core/settings')
-require('core/keybinds')
-
-require('plugins/nvim-tree')
-require('plugins/catppuccin')
-require('plugins/lualine')
--- update.sh
+++ update.sh
@@ -9,23 +9,23 @@ git_path="$HOME/git"
 echo "Copying files to git directory"
 
 # i3
-cpdir "$HOME/.config/i3" "$git_path/.config/i3"
+cpdir "$HOME/.config/i3" "$git_path/.config"
 
 # picom
-cpdir "$HOME/.config/picom" "$git_path/.config/picom"
+cpdir "$HOME/.config/picom" "$git_path/.config"
 
 # neovim
-cpdir "$HOME/.config/nvim" "$git_path/.config/nvim"
+cpdir "$HOME/.config/nvim" "$git_path/.config"
 
 # alacritty
-cpdir "$HOME/.config/alacritty" "$git_path/.config/alacritty"
+cpdir "$HOME/.config/alacritty" "$git_path/.config"
 cp "$HOME/tmux.conf" "$git_path/tmux.conf"
 
 # rofi
-cpdir "$HOME/.config/rofi" "$git_path/.config/rofi"
-cpdir "$HOME/.local/share/rofi" "$git_path/.local/share/rofi"
+cpdir "$HOME/.config/rofi" "$git_path/.config"
+cpdir "$HOME/.local/share/rofi" "$git_path/.local/share"
 
 # polybar
-cpdir "$HOME/.config/polybar" "$git_path/.config/polybar"
+cpdir "$HOME/.config/polybar" "$git_path/.config"
 
 echo "Done"
